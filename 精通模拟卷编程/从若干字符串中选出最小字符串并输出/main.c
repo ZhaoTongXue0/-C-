6d1c9d6d767d341cbd5569a39007b58451fbd852
赵同学 <96404999+ZhaoTongXue0@users.noreply.github.com>
2022-05-28 15:51:50 +0800

    2022年河北专升本计算机类专业课模拟试卷(一) 已整理完

--- 精通模拟卷编程/从若干字符串中选出最小字符串并输出/main.c
+++ 精通模拟卷编程/从若干字符串中选出最小字符串并输出/main.c
@@ -11,13 +11,13 @@
 char *strmin(char *name[5],int n){
     char *temp;
     int i;
-    temp=name[0];
+    temp=name[0];   //接受第1个字符串，用来当作最小值
     for (i=1; i<n; i++) {
-        if (strcmp(temp, name[i])>0) {
-            temp=name[i];
+        if (strcmp(temp, name[i])>0) {  //字符串比较要用strcmp指令 >结果是1;=结果是0;<结果是-1
+            temp=name[i];   //如果temp > name[i]的值，赋值 当做小值
         }
     }
-    return temp;
+    return temp; //返回temp 最小值
 }
 int main(){
     char *name[]={"Tom","Mary","Adam","Lance","Frank"};
